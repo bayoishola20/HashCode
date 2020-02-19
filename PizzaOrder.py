def69d399eae53fbbcd2b42f12e75420bc38ac58
Akolade Tinubu <aatinubu@users.noreply.github.com>
2020-02-19 04:53:21 +0100

    removed redundancies and add threading

--- PizzaOrder.py
+++ PizzaOrder.py
@@ -11,6 +11,7 @@
 
 
 import os
+from threading import Thread
 
 from ReadInputHandler import ReadInputHandler
 from WriteOutputHandler import WriteOutputHandler
@@ -18,7 +19,6 @@ from WriteOutputHandler import WriteOutputHandler
 
 class PizzaOrder:
     def __init__(self, input_file=None, output_file=None):
-
         if not os.path.isdir("output"):
             os.mkdir("output")
 
@@ -33,6 +33,16 @@ if __name__ == "__main__":
     input_files = ["a_example", "b_small",
                    "c_medium", "d_quite_big", "e_also_big"]
 
+    thread_list = []
+
     for i in range(len(input_files)):
-        PizzaOrder(f"input/{input_files[i]}.in",
-                   f"output/{input_files[i]}.out")
+        thread = Thread(target=PizzaOrder, args=(f"input/{input_files[i]}.in",
+                                                 f"output/{input_files[i]}.out"))
+        thread_list.append(thread)
+        thread.start()
+
+    for thread in thread_list:
+        thread.join()
+
+    # PizzaOrder(f"input/{input_files[i]}.in",
+    #            f"output/{input_files[i]}.out")
